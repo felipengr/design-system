36a3b660bd8340ca1dcb889ab8a85b5cff1b87c0
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-04-12 18:12:13 -0300

    Componente: Checkbox

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react'
+import { Check } from 'phosphor-react'
+import { CheckboxConteiner, CheckboxIndicator } from './styles'
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxConteiner> {}
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxConteiner {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxConteiner>
+  )
+}
