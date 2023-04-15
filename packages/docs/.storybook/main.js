bccea86d5d283b9ab74e35939f829d00c03700e1
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-04-15 16:16:42 -0300

    config in main js

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -17,5 +17,11 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system/'
+    }
+    return config
+  }
 };
 export default config;
